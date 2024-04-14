d7ed3653abc8e9a462c7ddea0de06a3c2a2aebe2
EmilAbdullayev <abdullayevemil27042006@gmail.com>
2024-04-15 02:32:55 +0400

    Configure Application

--- src/FitnessApp.Presentation/Program.cs
+++ src/FitnessApp.Presentation/Program.cs
@@ -1,22 +1,61 @@
+using FitnessApp.Core.Exercises.Repositories;
+using FitnessApp.Core.Food.Repositories;
+using FitnessApp.Core.News.Repositories;
+using FitnessApp.Core.SportSupplements.Repositories;
+using FitnessApp.Infrastructure.Data;
+using FitnessApp.Infrastructure.Exercises.Repositories;
+using FitnessApp.Infrastructure.Food.Repositories;
+using FitnessApp.Infrastructure.News.Repositories;
+using FitnessApp.Infrastructure.SportSupplements.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var infrastructureAssembly = typeof(FitnessAppDbContext).Assembly;
+
+builder.Services.AddMediatR(configurations =>
+{
+    configurations.RegisterServicesFromAssembly(infrastructureAssembly);
+});
+
+builder.Services.AddScoped<IExerciseRepository, ExerciseSqlRepository>();
+
+builder.Services.AddScoped<IFoodRepository, FoodSqlRepository>();
+
+builder.Services.AddScoped<INewsRepository, NewsSqlRepository>();
+
+builder.Services.AddScoped<ISportSupplementRepository, SportSupplementRepository>();
+
+builder.Services.AddAuthorization();
+
+var connectionString = builder.Configuration.GetConnectionString("FitnessDb");
+
+builder.Services.AddDbContext<FitnessAppDbContext>(dbContextOptionsBuilder =>
+{
+    dbContextOptionsBuilder.UseNpgsql(connectionString, o =>
+    {
+        o.MigrationsAssembly("FitnessApp.Presentation");
+    });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
+
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
